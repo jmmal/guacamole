2e7bd7bb579136d995f9db5f5b1ffa5b8a9141a6
Josh Maloney <dev@joshmaloney.com>
2020-09-06 12:18:36 +1000

    Fix build

--- web/src/app/activities/detailed-activity/detailed-activity.component.spec.ts
+++ web/src/app/activities/detailed-activity/detailed-activity.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailedActivityComponent } from './detailed-activity.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('DetailsActivityComponent', () => {
+describe('DetailedActivityComponent', () => {
   let component: DetailedActivityComponent;
   let fixture: ComponentFixture<DetailedActivityComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailedActivityComponent ]
+      declarations: [ DetailedActivityComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   }));
@@ -17,9 +19,5 @@ describe('DetailsActivityComponent', () => {
     fixture = TestBed.createComponent(DetailedActivityComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 });
