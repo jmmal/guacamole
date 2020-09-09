4fe1eed5b1d6440ec7d26ea2709d0899bba564e1
Josh Maloney <dev@joshmaloney.com>
2020-09-09 20:55:57 +1000

    Updates

---
+++ precache-manifest.8436ae0adacc791feacede74204129e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e007b8c74ceecfa8b39df630d1c0793d",
+    "url": "/guacamole/index.html"
+  },
+  {
+    "revision": "bfec8f1fcef8404cf094",
+    "url": "/guacamole/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "a34a632567db08ffafc2",
+    "url": "/guacamole/static/css/main.9ac7f03d.chunk.css"
+  },
+  {
+    "revision": "bfec8f1fcef8404cf094",
+    "url": "/guacamole/static/js/2.057bda20.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/guacamole/static/js/2.057bda20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34a632567db08ffafc2",
+    "url": "/guacamole/static/js/main.424f30b7.chunk.js"
+  },
+  {
+    "revision": "a8ccc81d4302bf1e4ecd",
+    "url": "/guacamole/static/js/runtime-main.2c7e92e3.js"
+  },
+  {
+    "revision": "83785ce4f6c09a3c72efd46ea7795f8f",
+    "url": "/guacamole/static/media/chevron-left.83785ce4.svg"
+  },
+  {
+    "revision": "2668a2328fde284b2372ef09ba796d89",
+    "url": "/guacamole/static/media/cloud-upload.2668a232.svg"
+  }
+]);
