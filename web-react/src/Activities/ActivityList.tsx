c0638e3e32d369f048470cab756d4d58f89127b3
Josh Maloney <dev@joshmaloney.com>
2020-09-09 20:36:04 +1000

    Add infinite scroll

--- web-react/src/Activities/ActivityList.tsx
+++ web-react/src/Activities/ActivityList.tsx
@@ -1,39 +1,53 @@
 import React, { useEffect, useState, ChangeEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { ReactComponent as CloudIcon } from 'bootstrap-icons/icons/cloud-upload.svg';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import { ActivityPreview } from './ActivityPreview';
 import { ActivityService } from './ActivityService';
 
-import { useHistory } from 'react-router-dom';
 import { Activity, ActivityTypeAggregation } from './models';
 import { Loading } from '../Shared';
 
 export const ActivityList = () => {
   const history = useHistory()
   const [activities, setActivities] = useState<Array<Activity>>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [page, setPage] = useState(1);
   const [filters, setFilters] = useState<Array<ActivityTypeAggregation>>([]);
 
   useEffect(() => {
-    getActivities(null)
+    getActivities(page, null).then(resp => {
+      updateActivities(resp.data.results, resp.data.totalCount)
+    })
 
     ActivityService.getFilters().then(resp => {
       setFilters(resp.data);
     })
-  }, [])
+  }, []);
 
-  function getActivities(filter: string | null) {
-    ActivityService
-      .getAllActivities(1, 20, filter)
-      .then(resp => {
-        setActivities(resp.data.results)
-      });
+  function updateActivities(activities: Activity[], count: number) {
+    setTotalCount(count);
+    setActivities(activities);
+  }
+
+  function getActivities(pageNumber: number = 1, filter: string | null) {
+    return ActivityService.getAllActivities(pageNumber, 3, filter);
   }
 
   function handleFilterChange(event: ChangeEvent<HTMLSelectElement>) {
     const filter = event.target.value;
+  }
 
-    getActivities(filter)
+  function loadNext() {
+    getActivities(page + 1, null).then(resp => {
+      updateActivities([
+        ...activities,
+        ...resp.data.results
+      ], resp.data.totalCount);
+      setPage(page + 1);
+    })
   }
 
   function onUploadClick() {
@@ -50,7 +64,7 @@ export const ActivityList = () => {
             onClick={onUploadClick}
           >Upload <CloudIcon />
           </button>
-          <select
+          {/* <select
             className="form-select type-filters"
             aria-label="Default select example"
             onChange={handleFilterChange}
@@ -59,15 +73,21 @@ export const ActivityList = () => {
             { filters && filters.map(filter => (
               <option key={filter.Name} value={filter.Name}>{ filter.Name } ({filter.Total})</option>
             ))}
-          </select>
+          </select> */}
         </div>
-        { activities && activities.length > 0 ? activities.map(activity => (
-          <ActivityPreview 
-            key={activity.id}
-            activity={activity}
-          />
-        )) : <Loading />
-        }
+        <InfiniteScroll
+          dataLength={activities.length}
+          next={loadNext}
+          hasMore={activities.length < totalCount}
+          loader={<Loading />}
+        >
+          { activities.map(activity => (
+            <ActivityPreview 
+              key={activity.id}
+              activity={activity}
+            />
+          ))}
+        </InfiniteScroll>
       </div>
     </div>
   )
