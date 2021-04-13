43382526c31ca062746b7fd7d1cfe71b37460264
Josh Maloney <dev@joshmaloney.com>
2021-04-13 20:08:41 +1000

    Update processor to find and replace existing docs

--- src/mongodb/write.ts
+++ src/mongodb/write.ts
@@ -1,14 +1,22 @@
-import { InsertOneWriteOpResult, WithId } from "mongodb";
+import { ReplaceOneOptions, ReplaceWriteOpResult } from "mongodb";
 import { Activity } from "../common/types";
 import { connectToDatabase } from "./connect";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const MONGODB_ACTIVITIES_COLLECTION = process.env.MONGODB_ACTIVITIES_COLLECTION;
 
-const writeActivity = async (activity: Activity): Promise<InsertOneWriteOpResult<WithId<Activity>>> => {
+const writeActivity = async (activity: Activity): Promise<ReplaceWriteOpResult> => {
   const db = await connectToDatabase(MONGODB_URI);
 
-  return db.collection(MONGODB_ACTIVITIES_COLLECTION).insertOne(activity);
+  const options: ReplaceOneOptions = {
+    upsert: true
+  };
+
+  const query = {
+    objectKey: activity.objectKey
+  };
+
+  return db.collection(MONGODB_ACTIVITIES_COLLECTION).replaceOne(query, activity, options);
 };
 
 export {
