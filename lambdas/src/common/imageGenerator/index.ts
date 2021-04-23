b58819cccf1c4a38ed24da1ce944768263d9174c
Josh Maloney <dev@joshmaloney.com>
2021-04-23 19:28:08 +1000

    Retry image generator if response is 413

--- lambdas/src/common/imageGenerator/index.ts
+++ lambdas/src/common/imageGenerator/index.ts
@@ -1,52 +1,61 @@
-import { MapiResponse } from '@mapbox/mapbox-sdk/lib/classes/mapi-response';
-import staticImageClient, { PathOverlay } from '@mapbox/mapbox-sdk/services/static';
+import staticImageClient, {
+  PathOverlay,
+} from '@mapbox/mapbox-sdk/services/static';
 import { GPSPoint } from '../types';
 import fetch from 'node-fetch';
+import { everyNthElement } from '../utils';
 
 const API_KEY = process.env.MAPBOX_API_KEY;
 const staticClient = staticImageClient({ accessToken: API_KEY });
 
-type LngLat = [number, number]
+type LngLat = [number, number];
 
-const getImage = (points: GPSPoint[]): Promise<Buffer> => {
-  const reduced = [];
-  const filtered = points.filter(Boolean);
-  for(let i = 0; i < filtered.length; i += 3) {
-    reduced.push(filtered[i]);
-  }
-
-  const coords = reduced.map<LngLat>(coord => {
+const getCoords = (points: GPSPoint[], precision: number) => {
+  const reduced = everyNthElement(points.filter(Boolean), precision);
+  return reduced.map<LngLat>((coord) => {
     return [coord.longitudeDegrees, coord.latitudeDegrees];
   });
+};
+const getImage = async (points: GPSPoint[]): Promise<Buffer> => {
+  let response;
+  let precision = 1;
+  do {
+    const coords = getCoords(points, precision);
+    const request = buildRequest(coords);
+    precision += 2;
 
+    // Sending the request with fetch rather than request.send() and static
+    // image client did not seem to return the right format
+    response = await fetch(request);
+  } while (response.status === 413);
+
+  return response.buffer();
+};
+
+const buildRequest = (coords: LngLat[]): string => {
   const pathOverlay: PathOverlay = {
     path: {
       coordinates: coords,
       strokeWidth: 4,
       strokeColor: '#FF6700',
-      strokeOpacity: 1
-    }
+      strokeOpacity: 1,
+    },
   };
-  
-  const request = staticClient.getStaticImage({
-    ownerId: 'mapbox',
-    styleId: 'outdoors-v11',
-    width: 1280,
-    height: 600,
-    highRes: true,
-    position: 'auto',
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    padding: '30',
-    overlays: [pathOverlay],
-  });
 
-  // Sending the request with fetch rather than request.send() and static 
-  // image client did not seem to return the right format
-  return fetch(request.url())
-    .then(resp => resp.buffer());
+  return staticClient
+    .getStaticImage({
+      ownerId: 'mapbox',
+      styleId: 'outdoors-v11',
+      width: 1280,
+      height: 600,
+      highRes: true,
+      position: 'auto',
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      padding: '30',
+      overlays: [pathOverlay],
+    })
+    .url();
 };
 
-export {
-  getImage
-};
+export { getImage };
--- lambdas/src/processActivityUpload/mapActivity/index.ts
+++ lambdas/src/processActivityUpload/mapActivity/index.ts
@@ -1,24 +1,34 @@
-import { SportsLib } from "@sports-alliance/sports-lib";
-import { DOMParser } from "xmldom";
-import { Activity } from "../../common/types";
-import { getImage } from "../../common/imageGenerator";
-import { uploadToBucket } from "../../common/storage";
-import { EventInterface } from "@sports-alliance/sports-lib/lib/events/event.interface";
-import { generateTitle, getPolyline, getStatOrNull, getStreamData } from "./utils";
+import { SportsLib } from '@sports-alliance/sports-lib';
+import { DOMParser } from 'xmldom';
+import { Activity } from '../../common/types';
+import { getImage } from '../../common/imageGenerator';
+import { uploadToBucket } from '../../common/storage';
+import { EventInterface } from '@sports-alliance/sports-lib/lib/events/event.interface';
+import {
+  generateTitle,
+  getPolyline,
+  getStatOrNull,
+  getStreamData,
+} from './utils';
 
 const S3_URL = process.env.S3_URL;
 
-const mapFileToActivity = async (filename: string, file: Buffer): Promise<Activity> => {
+const mapFileToActivity = async (
+  filename: string,
+  file: Buffer
+): Promise<Activity> => {
   let event: EventInterface;
   const filestring = file.toString();
 
   try {
-    if (filename.match(".fit$")) {
+    if (filename.match('.fit$')) {
       event = await SportsLib.importFromFit(file);
-    } else if (filename.match(".gpx$")) {
-      event = await SportsLib.importFromGPX(filestring, DOMParser); 
-    } else if (filename.match(".tcx$")) {
-      event = await SportsLib.importFromTCX((new DOMParser()).parseFromString(filestring, 'application/xml'));
+    } else if (filename.match('.gpx$')) {
+      event = await SportsLib.importFromGPX(filestring, DOMParser);
+    } else if (filename.match('.tcx$')) {
+      event = await SportsLib.importFromTCX(
+        new DOMParser().parseFromString(filestring, 'application/xml')
+      );
     } else {
       return null;
     }
@@ -26,16 +36,17 @@ const mapFileToActivity = async (filename: string, file: Buffer): Promise<Activi
     return null;
   }
 
-
   const distance = event.getDistance().getValue();
   const duration = event.getDuration().getValue();
 
   // TODO: Support multi activity files
   const activity = event.getFirstActivity();
 
-  const pauseTime = getStatOrNull(activity, "Pause Time") as number;
-  const movingTime = getStatOrNull(activity, "Moving time") as number;
-  const positionData = activity.hasPositionData() ? activity.getPositionData() : [];
+  const pauseTime = getStatOrNull(activity, 'Pause Time') as number;
+  const movingTime = getStatOrNull(activity, 'Moving time') as number;
+  const positionData = activity.hasPositionData()
+    ? activity.getPositionData()
+    : [];
   const streamData = getStreamData(activity);
 
   let imageBuffer: Buffer;
@@ -50,23 +61,26 @@ const mapFileToActivity = async (filename: string, file: Buffer): Promise<Activi
 
   return {
     objectKey: filename,
-    title: activity.name === '' ? generateTitle(activity.startDate, activity.type) : activity.name, 
+    title:
+      activity.name === ''
+        ? generateTitle(activity.startDate, activity.type)
+        : activity.name,
     type: activity.type,
     startTime: event.startDate,
     endTime: event.endDate,
     pace: {
-      avg: getStatOrNull(activity, "Average Pace"),
-      min: getStatOrNull(activity, "Minimum Pace"),
-      max: getStatOrNull(activity, "Maximum Pace"),
+      avg: getStatOrNull(activity, 'Average Pace'),
+      min: getStatOrNull(activity, 'Minimum Pace'),
+      max: getStatOrNull(activity, 'Maximum Pace'),
     },
     elapsedTime: (movingTime ?? 0) + (pauseTime ?? 0),
     movingTime: movingTime,
     polyline: getPolyline(positionData),
     simplePolyline: getPolyline(positionData, true),
     elevation: {
-      min: getStatOrNull(activity, "Minimum Altitude"),
-      max: getStatOrNull(activity, "Maximum Altitude"),
-      avg: getStatOrNull(activity, "Average Altitude"),
+      min: getStatOrNull(activity, 'Minimum Altitude'),
+      max: getStatOrNull(activity, 'Maximum Altitude'),
+      avg: getStatOrNull(activity, 'Average Altitude'),
     },
     calories: getStatOrNull(activity, 'Energy'),
     ascent: getStatOrNull(activity, 'Ascent'),
@@ -75,15 +89,13 @@ const mapFileToActivity = async (filename: string, file: Buffer): Promise<Activi
     duration,
     points: positionData,
     heartRate: {
-      max: getStatOrNull(activity, "Maximum Heart Rate"),
-      min: getStatOrNull(activity, "Minimum Heart Rate"),
-      avg: getStatOrNull(activity, "Average Heart Rate"),
+      max: getStatOrNull(activity, 'Maximum Heart Rate'),
+      min: getStatOrNull(activity, 'Minimum Heart Rate'),
+      avg: getStatOrNull(activity, 'Average Heart Rate'),
     },
     streamData,
-    imageURL: imageName ? S3_URL + imageName : null
+    imageURL: imageName ? S3_URL + imageName : null,
   };
 };
 
-export {
-  mapFileToActivity
-};
+export { mapFileToActivity };
