99f8a409052004cb8bed6272b70fbc0639460034
Josh Maloney <dev@joshmaloney.com>
2021-04-25 20:39:58 +1000

    Fix baseUrl

--- client/src/Stats/useStats.ts
+++ client/src/Stats/useStats.ts
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import { calculateStats } from '../Shared/utils';
 import { StatsSummary } from '../Shared/types';
 
+const baseUrl = process.env.REACT_APP_BASE_API_URL;
+
 export const useStats = () => {
   const [stats, setStats] = useState<StatsSummary>();
 
   useEffect(() => {
-    fetch('/activities/stats')
+    fetch(baseUrl + '/activities/stats')
       .then((resp) => resp.json())
       .then((json) => {
         const stats = calculateStats(json);
