f18ca75ee146db3650d66ddb734e9b322e9a1889
Josh Maloney <dev@joshmaloney.com>
2022-01-26 16:17:54 +1100

    Export icon

--- client/src/Shared/icons/Previous.tsx
+++ client/src/Shared/icons/Previous.tsx
@@ -1,4 +1,4 @@
-export default () => {
+const Previous = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,3 +17,5 @@ export default () => {
     </svg>
   );
 };
+
+export default Previous;
